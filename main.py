e6792127bf6cc696edec1a1984b0b5bdcd03bfbe
Misha Sahan <mishkasahan@gmail.com>
2023-12-15 00:12:56 +0200

    IndexError

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+a = [1,3,4,5]
+
 try:
-    a = int(input("Введіть перше число:"))
-    b = int(input("Введіть друге число:"))
-except ValueError:
-    print("Нажаль Ви ввели не число!")
+    b = a[6]
+    print(b)
+except IndexError:
+    print('Ви звертаєтесь до неіснуючого індексу')
 
