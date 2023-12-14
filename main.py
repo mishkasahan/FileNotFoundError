c704bc4eec9ec40fcd9601d65a55de4c4810e35f
Misha Sahan <mishkasahan@gmail.com>
2023-12-15 00:07:46 +0200

    ValueError

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import os
-
-filename = 'text.txt'
-abs_path = os.path.abspath(__file__)
-print(abs_path)
-
-dir_path = os.path.dirname(abs_path)
-print(dir_path)
-
-file_path = os.path.join(dir_path,'ppka',filename)
-print(file_path)
-
 try:
-    f = open(file_path, encoding='utf-8').read(10)
-    print(f)
-except FileNotFoundError:
-    print('Файл не знайдено')
+    a = int(input("Введіть перше число:"))
+    b = int(input("Введіть друге число:"))
+except ValueError:
+    print("Нажаль Ви ввели не число!")
+
